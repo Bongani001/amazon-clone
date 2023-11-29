8820dfc63f156d5018abeca0e1a4743ce82e820e
Bongani <bbongs421@gmail.com>
2023-11-29 19:28:35 +0200

    Add items in the display

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,18 @@
 import React from "react";
 import "./Product.css";
 
-const Product = ({id, title, image, rating, price}) => {
+const Product = ({ id, title, image, rating, price }) => {
   return (
     <div className="product">
-      <img
-        src={image}
-        alt=""
-      />
+      <img src={image} alt="" />
       <div className="product_info">
-        <p>
-          {title}
-        </p>
+        <p>{title}</p>
+        <div className="product_rating">
+          <p>{rating}</p>
+        </div>
+        <p className="product_price">{price}</p>
       </div>
-      <div className="product_rating">
-        <p>{rating}</p>
-      </div>
-      <p className="product_price">{price}</p>
+      <button className="product_button">Add to Basket</button>
     </div>
   );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Products.css"
+import "./Products.css";
 import Product from "./Product";
 
 const Products = () => {
   return (
-    <div className="products_row">
-      <Product id="1" title="Seagate Portable 2TB External Hard Drive Portable HDD - USB 3.0 for
-          PC, Mac, PlayStation, & Xbox - 1-Year Rescue Service (STGX2000400)" image="https://m.media-amazon.com/images/I/81tjLksKixL._AC_UL320_.jpg" rating="4" price="59.99" />
-    </div>
+    <>
+      <div className="products_row">
+        <Product
+          id="1"
+          title="Seagate Portable 2TB External Hard Drive Portable HDD - USB 3.0 for
+          PC, Mac, PlayStation, & Xbox - 1-Year Rescue Service (STGX2000400)"
+          image="https://m.media-amazon.com/images/I/81tjLksKixL._AC_UL320_.jpg"
+          rating="4"
+          price="59.99"
+        />
+        <Product
+          id="1234567"
+          title="Razer Cynosa Chroma Gaming Keyboard: Individually Backlit RGB Keys - Spill-Resistant Design - Programmable Macro Functionality - Quiet & Cushioned"
+          image="https://images-na.ssl-images-amazon.com/images/I/61WgmLgwuQL._AC_SX300_SY300_QL70_FMwebp_.jpg"
+          rating="5"
+          price="34.99"
+        />
+      </div>
+      <div className="products_row">
+        <Product
+          id="1434569"
+          title="AMD Ryzen 5 5600X 6-core, 12-Thread unlocked Desktop Processor with Wraith Stealth Cooler"
+          image="https://m.media-amazon.com/images/I/61vGQNUEsGL._AC_UL320_.jpg"
+          rating="5"
+          price="288.89"
+        />
+        <Product
+          id="12365487"
+          title="Soundance Laptop Stand, Aluminium Computer Riser, Ergomomiclaptops Elevator for Desk, Metal Holder Compatible with 10 to 15.6 Inches Notebook Computer, Silver"
+          image="https://m.media-amazon.com/images/I/81MN9l2nl2S._AC_UL320_.jpg"
+          rating="2"
+          price="27.99"
+        />
+        <Product
+          id="12345567"
+          title="H390 Wired Headset, Sterio Headphones with Noise-Cancelling Microphone"
+          image="https://m.media-amazon.com/images/I/61CqYq+xwNL._AC_UL320_.jpg"
+          rating="5"
+          price="24.99"
+        />
+      </div>
+      <div className="products_row">
+        <Product
+          id="12345367"
+          title="Sceptre 24 inch Professional Thin 75Hz 1080p LED Monitor 2x HDMI VGA Built-in Speakers, Machine Black (E248W-19203R Series)"
+          image="https://m.media-amazon.com/images/I/71rXSVqET9L._AC_UL320_.jpg"
+          rating="3"
+          price="193.49"
+        />
+      </div>
+    </>
   );
 };
 
