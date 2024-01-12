7937a502d31a988340380aa7ce399b9313739904
Bongani <bbongs421@gmail.com>
2024-01-12 15:09:54 +0200

    fix order page

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,44 +1,42 @@
 import React, { useState, useEffect, useContext } from "react";
-import { db } from "../firebase";
-import "./Orders.css";
 import ShoppingContext from "../context/shopping/shoppingContext";
 import Order from "./Order";
+import "./Orders.css";
+import { db } from "../firebase";
 
-function Orders() {
+const Orders = () => {
   const shoppingContext = useContext(ShoppingContext);
   const { user } = shoppingContext;
+
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     if (user) {
-      db.collection("users")
+      db.collection("user")
         .doc(user?.uid)
         .collection("orders")
         .orderBy("created", "desc")
-        .onSnapshot((snapshot) =>
+        .onSnapshot((snapshot) => {
           setOrders(
-            snapshot.docs.map((doc) => ({
-              id: doc.id,
-              data: doc.data(),
-            }))
-          )
-        );
+            snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+          );
+        });
     } else {
       setOrders([]);
     }
   }, [user]);
 
+  console.log(orders);
   return (
     <div className="orders">
       <h1>Your Orders</h1>
-
-      <div className="orders__order">
+      <div className="orders_order">
         {orders?.map((order) => (
-          <Order order={order} />
+          <Order key={order.id} order={order} />
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default Orders;
